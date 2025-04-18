ea33042361ccd20f31a47eaf13d88d4372584379
Oskar Przybylski <oskar.przybylski23@gmail.com>
2025-04-18 19:42:59 +0100

    add Spinner component

---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type SpinnerProps = {
+  text?: string;
+  className?: string;
+  visible?: boolean;
+};
+
+export default function Spinner({
+  text = '',
+  className = '',
+  visible = true,
+}: SpinnerProps) {
+  if (!visible) return null;
+
+  return (
+    <div className={className}>
+      <img src='assets/spinner.svg' className='spinner' alt='Loading...' />
+      <span>{text}</span>
+    </div>
+  );
+}
